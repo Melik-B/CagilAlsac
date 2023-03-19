6b9446f3a472a5893f8406b304d3d6a776a27a3c
Melik <melikbaykal06@gmail.com>
2023-03-19 16:14:40 +0300

    Add project files.

---
+++ CagilAlsac/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CagilAlsac.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
